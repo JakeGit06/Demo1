32166112f6c121e2a4a94efcccbecd818fced572
Jakepc <Jakepc@DESKTOP-ICCV887>
2023-06-14 12:34:32 +0530

    Commit1

---
+++ SeleniumTestExecution/src/test/java/TestExecution/NewTest.java
@@ -0,0 +1,19 @@
+package TestExecution;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void Testcase1() {
+	  System.setProperty("webdriver.chrome.driver","D:\\Browser Drivers\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("single-input-field")).sendKeys("Hello");
+		driver.findElement(By.xpath("//button[@id='button-one']")).click();
+  }
+}
